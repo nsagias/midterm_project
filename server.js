7de288064a19c84f56426e30daa78ea662f7d6a0
Nick <nsagias@gmail.com>
2021-10-18 15:10:45 -0400

    Change directory to images for development, public is on gitignore

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(morgan("dev"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static('images'));
 
 app.use(
   "/styles",

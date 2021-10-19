f91bb5b5e6a5e2f8a10e091dfb1a48723845b37a
Nick <nsagias@gmail.com>
2021-10-19 00:41:45 -0400

    Create favourites schema and messages schema

---
+++ db/schema/03_favourites.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS favourites CASCADE;
+
+CREATE TABLE favourites (
+  id SERIAL PRIMARY KEY NOT NULL,
+  favourite_date DATE NOT NULL,
+  car_id   INTEGER REFERENCES cars(id)  ON DELETE CASCADE,
+  buyer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  favourite_bool BOOLEAN NOT NULL DEFAULT FALSE
+);
---
+++ db/schema/04_messages.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS messages  CASCADE;
+
+CREATE TABLE messages (
+  id SERIAL PRIMARY KEY NOT NULL,
+  sent_date DATE NOT NULL,
+  email_id_sender    VARCHAR(255) NOT NULL,
+  email_id_receiver  VARCHAR(255) NOT NULL,
+  seller_id INTEGER,
+  car_id    INTEGER REFERENCES cars(id)  ON DELETE CASCADE,
+  buyer_id  INTEGER REFERENCES users(id) ON DELETE CASCADE
+);

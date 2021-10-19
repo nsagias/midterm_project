986bd4a4095366f82dbc6a3e9e2f7b53127b5bf6
Parris Greaves <parris.greaves@gmail.com>
2021-10-19 13:26:27 -0400

    add users schema and seeds, update messages to align with users

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,8 +1,10 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alicia-BOB');
-INSERT INTO users (name) VALUES ('Jacob');
-INSERT INTO users (name) VALUES ('Johns');
-INSERT INTO users (name) VALUES ('Maira');
-INSERT INTO users (name) VALUES ('Kira');
-INSERT INTO users (name) VALUES ('Ed');
-INSERT INTO users (name) VALUES ('Bobby');
+INSERT INTO users (name, email, password, phone, admin)
+VALUES
+  ('Andy Apple', 'apple@gmail.com', 'password', '519-303-3030', FALSE),
+  ('Julie Orange', 'orange@gmail.com', 'password', '647-898-9898', FALSE),
+  ('Manny Mango', 'mango@gmail.com', 'password', '123-456-7890', FALSE),
+  ('Peter Pear', 'pear@gmail.com', 'password', '345-543-3456', FALSE),
+  ('Gary Grape', 'grape@gmail.com', 'password', '234-432-2341', FALSE),
+  ('Penny Pineapple', 'pineApple@gmail.com', 'password', '567-897-6787', FALSE),
+  ('Adam Administrator', 'aadmin@gmail.com', 'password', '519-999-9595', TRUE);
--- db/seeds/04_messages.sql
+++ db/seeds/04_messages.sql
@@ -8,15 +8,15 @@
 --   car_id    INTEGER REFERENCES cars(id)  ON DELETE CASCADE,
 --   buyer_id  INTEGER REFERENCES users(id) ON DELETE CASCADE
 -- );
---  NOTE: SELLER AND BUYERS CANNOT BE THE SAME, 
+--  NOTE: SELLER AND BUYERS CANNOT BE THE SAME,
 --  NOTE: EMAILS CANNOT BE THE SAME AS WELL
-INSERT INTO 
-  messages 
+INSERT INTO
+  messages
   (sent_date, email_id_sender, email_id_receiver, seller_id, car_id, buyer_id)
 VALUES
-  ('2021-10-15', 'apple@gmail.com', 'notApple@gmail.com'           , 5, 1, 3),
-  ('2021-10-15', 'pear@gmail.com', 'notPear@gmail.com'             , 5, 2, 3),
-  ('2021-10-17', 'orange@gmail.com', 'notOrange@gmail.com'         , 5, 2, 3),
-  ('2021-10-17', 'mango@gmail.com', 'notmang@gmail.com'            , 5, 2, 3),
-  ('2021-10-18', 'pineApple@gmail.com', 'notPineApple@gmail.com'   , 4, 3, 1),
-  ('2021-10-19', 'grape@gmail.com', 'notGrape@gmail.com'           , 4, 1, 2);
+  ('2021-10-15', 'apple@gmail.com', 'mango@gmail.com',            3, 1, 1),
+  ('2021-10-15', 'pear@gmail.com', 'mango@gmail.com' ,            3, 2, 4),
+  ('2021-10-17', 'orange@gmail.com', 'mango@gmail.com',           3, 2, 2),
+  ('2021-10-17', 'mango@gmail.com', 'grape@gmail.com',            5, 2, 3),
+  ('2021-10-18', 'pineApple@gmail.com', 'apple@gmail.com',         1, 3, 6),
+  ('2021-10-19', 'grape@gmail.com', 'orange@gmail.com' ,           2, 1, 5);

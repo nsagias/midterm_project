5decfe03c02c1636b1005d5cacee9e10c8f61bc3
jenobpj <jenobgit@gmail.com>
2021-10-21 09:32:11 -0400

    add admin nodemailer message service

--- server.js
+++ server.js
@@ -90,6 +90,34 @@ app.post('/messages',async(req,res)=>{
   res.send("email has send")
 })
 
+app.post('/adminmessage',async(req,res)=>{
+  const{email,emailContent,subject}=req.body;
+  console.log(req.body.email)
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'bessie.schiller54@ethereal.email', // generated ethereal user
+      pass: 'NtKGqxXMqAPCn4tzQQ', // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: 'bessie.schiller54@ethereal.email', // sender address
+    to: `${email}`, // list of receivers
+    subject: `${subject}`, // Subject line
+    text:`${emailContent}`, // plain text body // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  res.send("email has send")
+})
 
 // get all cars function
 const getAllCars = function(req, resp) {

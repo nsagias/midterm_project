b884aa0bf22a6febec4dc44cbc2f67a89decf97c
Nick <nsagias@gmail.com>
2021-10-22 15:04:44 -0400

    remove unused boilerplate code

--- server.js
+++ server.js
@@ -52,10 +52,7 @@ const widgetsRoutes = require("./routes/widgets");
 const { Template } = require("ejs");
 const { isDate } = require("moment");
 
-// Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+
 // Note: mount other resources here, using the same pattern above
 
 

559f81da25ee06c6d7f5677c1edc4d9d410f509d
Nick <nsagias@gmail.com>
2021-10-22 14:56:49 -0400

    Remove developments notes comment out some boilerplate but keep for in production testing

--- server.js
+++ server.js
@@ -129,11 +129,8 @@ app.post('/messages',async(req,res)=>{
     text:`Sender Email: ${sender_email} \nMessage: ${emailContent}`, 
   });
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // console.log("Message sent: %s", info.messageId);
+  // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   res.redirect("/cars");
 });
 
